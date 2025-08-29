672e64e0ae5c21fde4dde8ad6ffe6de59c6bf038
Yoga Prasetya <yogaajie2@gmail.com>
2025-08-29 18:31:29 +0700

    types: add more type keys

--- src/types.ts
+++ src/types.ts
@@ -1,5 +1,10 @@
 export interface Pokemon {
+  abilities: { ability: { name: string } }[];
+  height: number;
   id: number;
+  moves: { move: { name: string } }[];
   name: string;
+  stats: { base_stat: number; stat: { name: string } }[];
   types: { type: { name: string } }[];
+  weight: number;
 }
